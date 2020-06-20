5e90347ddec5a1793ea87e5e8ece301917d20981
Dmytro Petrovskiy <dmytro.petrovskiy@slice.com>
2020-06-20 21:18:51 +0300

    W

--- my_solutions/hw2_q2_2d_made.py
+++ my_solutions/hw2_q2_2d_made.py
@@ -27,88 +27,78 @@ class PairsDataset(Dataset):
         return len(self.data)
 
 
-class Made2DV0(torch.nn.Module):
-    def __init__(self, d, emb_dim=10, hidden_dim=10):
-        super().__init__()
-        self.emb_dim = emb_dim
-        self.hidden_dim = hidden_dim
-
-        self.emb0 = torch.nn.Embedding(d, embedding_dim=emb_dim)
-        self.emb1 = torch.nn.Embedding(d, embedding_dim=emb_dim)
-
-        self.h0 = torch.nn.Linear(emb_dim, hidden_dim)
-        self.h1 = torch.nn.Linear(emb_dim, hidden_dim)
-
-        self.out1 = torch.nn.Linear(2 * hidden_dim, d)
-        self.out2 = torch.nn.Linear(2 * hidden_dim, d)
-
-    def forward(self, *input):
-        x = input[0]
-
-        x0 = x[:, 0]
-        x1 = x[:, 1]
-
-        x0 = self.emb0(x0)
-        x1 = self.emb1(x1)
-
-        h0 = self.h0(torch.zeros_like(x0))
-        h0 = F.relu(h0)
-
-        h1 = self.h1(x0)
-        h1 = F.relu(h1)
-
-        out0 = torch.cat([h0, torch.zeros_like(h1)], dim=-1)
-
-
 class Made2D(torch.nn.Module):
     def __init__(self, d, emb_dim=10, hidden_dim=10):
         super().__init__()
+        self.d = d
         self.emb_dim = emb_dim
         self.hidden_dim = hidden_dim
 
         self.emb = torch.nn.Embedding(d, embedding_dim=emb_dim)
 
-        self.h = torch.nn.Parameter(torch.zeros(hidden_dim, emb_dim))
+        self.h = torch.nn.Parameter(torch.zeros(hidden_dim, 2 * emb_dim))
         self.out0 = torch.nn.Parameter(torch.zeros(d, hidden_dim))
+        self.out0_bias = torch.nn.Parameter(torch.zeros(d, ))
         self.out1 = torch.nn.Parameter(torch.zeros(d, hidden_dim))
+        self.out1_bias = torch.nn.Parameter(torch.zeros(d, ))
 
         torch.nn.init.normal_(self.h.data)
         torch.nn.init.normal_(self.out0.data)
         torch.nn.init.normal_(self.out1.data)
+        torch.nn.init.normal_(self.out0_bias.data)
+        torch.nn.init.normal_(self.out1_bias.data)
 
-        self.h_mask = torch.zeros((hidden_dim, emb_dim))
+        self.h_mask = torch.zeros((hidden_dim, 2 * emb_dim))
         self.out0_mask = torch.zeros((d, hidden_dim))
         # self.out1_mask = torch.zeros((d, hidden_dim))
 
-        self.h_mask[hidden_dim / 2:, :emb_dim / 2] = 1
-        self.out0_mask[:, :hidden_dim / 2] = 1
+        self.h_mask[hidden_dim // 2:, :emb_dim] = 1
+        self.out0_mask[:, :hidden_dim // 2] = 1
 
         self.criterion = CrossEntropyLoss()
 
+    def get_distribution(self):
+        with torch.no_grad():
+            inp = torch.cartesian_prod(torch.arange(self.d), torch.arange(self.d))
+            s0, s10 = self.get_scores([inp])
+
+            p0 = F.softmax(s0, dim=1)
+            p10 = F.softmax(s10, dim=1)
+
+            i0 = inp[:, 0]
+            p0 = p0[torch.arange(p0.shape[0]), i0]
+            p10 = p10[torch.arange(p10.shape[0]), i0]
+
+            return p0 * p10
+
     def forward(self, *input):
-        x = input[0]
         target = input[0]
-
-        x0 = x[:, 0]
-        x1 = x[:, 1]
-
-        x0 = self.emb(x0)
-        x1 = self.emb(x1)
-
-        x = torch.cat([x0, x1], dim=-1)
-        x = ((self.h * self.h_mask) @ x.T).T
-        x = F.relu(x)
-
-        out0 = ((self.out0 * self.out0_mask) @ x.T).T
-        out1 = (self.out1 @ x.T).T
-
         t0 = target[:, 0]
         t1 = target[:, 1]
+
+        out0, out1 = self.get_scores(input)
 
         loss0 = self.criterion(out0, t0)
         loss1 = self.criterion(out1, t1)
 
         return (loss0 + loss1) / 2
+
+    def get_scores(self, input):
+        x = input[0]
+        x0 = x[:, 0]
+        x1 = x[:, 1]
+        x0 = self.emb(x0)
+        x1 = self.emb(x1)
+        x = torch.cat([x0, x1], dim=-1)
+        x = ((self.h * self.h_mask) @ x.T).T
+        x = F.relu(x)
+        out0 = ((self.out0 * self.out0_mask) @ x.T).T
+        out0 = out0 + self.out1_bias
+
+        out1 = (self.out1 @ x.T).T
+        out1 = out1 + self.out1_bias
+
+        return out0, out1
 
 
 def sample_data(dset_type):
@@ -138,6 +128,43 @@ def sample_data(dset_type):
     return train_dist, test_dist, train_data, test_data
 
 
+def train_loop(model, train_data, test_data, epochs=100, batch_size=64):
+    opt = Adam(model.parameters(), lr=1e-3)
+
+    train_ds = PairsDataset(train_data)
+    test_ds = PairsDataset(test_data)
+
+    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
+
+    losses = []
+    test_losses = []
+    for e in tqdm(range(epochs)):
+        for b in train_loader:
+            loss = model(b)
+            # loss += (-model.s.flatten()).relu().sum()
+            loss.backward()
+            # print(get_grad_norm(model))
+            opt.step()
+            losses.append(loss.detach().numpy().item())
+
+            opt.zero_grad()
+
+            # s = model.s.data.detach()
+            # model.s = torch.nn.Parameter(s)
+
+        with torch.no_grad():
+            tmp = []
+            for b in test_loader:
+                loss = model(b)
+                tmp.append(loss.numpy())
+
+            test_losses.append(np.mean(tmp))
+            print(test_losses[-1])
+
+    return losses, test_losses
+
+
 def q2_a(train_data, test_data, d, dset_id):
     """
     train_data: An (n_train, 2) numpy array of integers in {0, ..., d-1}
@@ -158,19 +185,25 @@ def q2_a(train_data, test_data, d, dset_id):
 if __name__ == '__main__':
     # visualize_q2a_data(dset_type=1)
 
+    d = 25
     batch_size = 64
     train_dist, test_dist, train_data, test_data = sample_data(1)
-    train_ds = PairsDataset(train_data)
-    test_ds = PairsDataset(test_data)
+    # train_ds = PairsDataset(train_data)
+    # test_ds = PairsDataset(test_data)
+    #
+    # train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    # test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
+    #
+    # bsz = 4
+    # emb_d = 5
+    # h_d = 3
+    # h = torch.nn.Parameter(torch.zeros((h_d, emb_d)))
+    # torch.nn.init.xavier_uniform_(h.data)
+    # x = torch.rand((bsz, emb_d))
+    #
+    # res = (h @ x.T).T
 
-    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
+    model = Made2D(d)
 
-    bsz = 4
-    emb_d = 5
-    h_d = 3
-    h = torch.nn.Parameter(torch.zeros((h_d, emb_d)))
-    torch.nn.init.xavier_uniform_(h.data)
-    x = torch.rand((bsz, emb_d))
-
-    res = (h @ x.T).T
+    losses, test_losses = train_loop(model, train_data, test_data, epochs=2)
+    distribution = model.get_distribution()
