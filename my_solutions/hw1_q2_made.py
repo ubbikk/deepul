e668b5eb0b5ab1a8f598b9b30d5ff1faf17add3e
Dmytro Petrovskiy <dmytro.petrovskiy@slice.com>
2020-06-29 13:49:26 +0300

    W

---
+++ my_solutions/hw1_q2_made.py
@@ -0,0 +1,6 @@
+from torch.nn.modules.loss import CrossEntropyLoss
+from torch.optim import Adam
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+
+from deepul.hw1_helper import *
