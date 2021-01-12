06e65d374958ec429e51e6817304d9fd53dc8a08
Dmytro Petrovskiy <dd.petrovskiy@gmail.com>
2021-01-12 17:39:20 +0200

    added

--- my_solutions/hw2_q3_real_nvp.py
+++ my_solutions/hw2_q3_real_nvp.py
@@ -80,18 +80,20 @@ class SimpleResnet(torch.nn.Module):
         x = self.conv2(x)
         return x
 
+
 class AffineCouplingLayer(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        # self.register_buffer('mask', mask)
-        self.resnet = SimpleResnet(in_channels=3, out_channels=3*2) # ?
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.resnet = SimpleResnet(in_channels=in_channels, out_channels=2*self.out_channels)  # ?
         self.scale = torch.nn.Parameter(torch.ones(1))
         self.scale_shift = torch.nn.Parameter(torch.zeros(1))
 
     def forward(self, x, mask):
         s, t = torch.chunk(self.resnet(x * mask), 2, dim=1)
         # calculate log_scale, as done in Q1(b)
-        log_scale = self.scale*torch.tanh(s)+self.scale_shift
+        log_scale = self.scale * torch.tanh(s) + self.scale_shift
 
         t = t * (1.0 - mask)
         log_scale = log_scale * (1.0 - mask)
@@ -99,6 +101,17 @@ class AffineCouplingLayer(torch.nn.Module):
         log_det_jacobian = log_scale
         return z, log_det_jacobian
 
+    def invert(self, z, mask):
+        with torch.no_grad():
+            s, t = torch.chunk(self.resnet(z * mask), 2, dim=1)
+            log_scale = self.scale * torch.tanh(s) + self.scale_shift
+
+            t = t * (1.0 - mask)
+            log_scale = log_scale * (1.0 - mask)
+
+            x = (z-t)*torch.exp(-log_scale)
+
+            return x
 
 
 def q3_a(train_data, test_data):
